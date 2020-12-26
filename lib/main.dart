aa02b899050d7a5a73e6c86d631bbd50ce0a502a
ewajs <ezequiel.wajs@gmail.com>
2020-12-26 13:42:14 -0300

    Adding a Drawer to switch between categories and filters

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/tabs_screen.dart';
+import './screens/filter_screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,6 +37,7 @@ class MyApp extends StatelessWidget {
         '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       // This will be executed if we go to a named route not present in
       // the route table above.
