9c820637a9331c61de2e0cb93c14a73d4f371c5e
ewajs <ezequiel.wajs@gmail.com>
2020-12-26 13:06:25 -0300

    Added favorites screen and switched initial screen to be a tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'package:meals/screens/category_meals_screen.dart';
-import 'package:meals/screens/meal_detail_screen.dart';
-import 'screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/categories_screen.dart';
+import './screens/tabs_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ))),
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
